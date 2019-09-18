f7f97efee2959903b491250baf69f2554f77a629
tommy <tommyyun96@gmail.com>
2019-09-18 02:06:00 -0400

    Updates

---
+++ precache-manifest.560f34480e8d601d470b93bb82519139.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ecaedc8127ffe8042daadc4a8dd2748",
+    "url": "/BOBO/index.html"
+  },
+  {
+    "revision": "c22bbfa7b8f112cc6abc",
+    "url": "/BOBO/static/css/2.09d751c7.chunk.css"
+  },
+  {
+    "revision": "6b927bda3eb0ee0f9375",
+    "url": "/BOBO/static/css/main.2d4a9ecf.chunk.css"
+  },
+  {
+    "revision": "c22bbfa7b8f112cc6abc",
+    "url": "/BOBO/static/js/2.e3df0c92.chunk.js"
+  },
+  {
+    "revision": "6b927bda3eb0ee0f9375",
+    "url": "/BOBO/static/js/main.7e658691.chunk.js"
+  },
+  {
+    "revision": "d5885e5e58f947cf402e",
+    "url": "/BOBO/static/js/runtime~main.8e648452.js"
+  },
+  {
+    "revision": "3a739164985ecfb2dcb4283eca1cff39",
+    "url": "/BOBO/static/media/BOBO_logo.3a739164.jpeg"
+  },
+  {
+    "revision": "38eea7b4785ba7aa1f78997b373f63e9",
+    "url": "/BOBO/static/media/basic.38eea7b4.png"
+  }
+]);
